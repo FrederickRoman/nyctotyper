e5e7e4953f4f0138c0abae3d318660d46c1fb837
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-04-03 02:54:41 -0700

    feat(App): Add SiteFooter

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
   import HeroBanner from "./components/banner/HeroBanner.svelte";
   import Nyctography from "./components/nyctograpy/Nyctography.svelte";
+  import SiteFooter from "./components/footer/SiteFooter.svelte";
 </script>
 
 <main>
   <HeroBanner />
   <Nyctography />
+  <SiteFooter />
 </main>
---
+++ src/components/footer/SiteFooter.svelte
@@ -0,0 +1,25 @@
+<script>
+  const currentYear = new Date().getFullYear();
+</script>
+
+<footer>
+  <a
+    href="https://www.frederickroman.com/"
+    target="_blank"
+    rel="noopener"
+    class="link"
+  >
+    &copy; Frederick Roman {currentYear}
+  </a>
+</footer>
+
+<style>
+  footer {
+    text-align: center;
+    padding: 20px;
+  }
+  .link {
+    color: #c4b1b1;
+    text-decoration: none;
+  }
+</style>
